9c86105ade7c8608a42b58034ddc6dae495028c2
Kushal Bhattarai <kushalbhattarai100@gmail.com>
2026-07-20 21:18:17 +0545

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-app = FastAPI()
 
 # Stage-0
+#app = FastAPI()
+
 # @app.get("/")
 
 # def read_root():
@@ -13,17 +14,51 @@ app = FastAPI()
 # read_root()
 
 #stage-1
+#app = FastAPI()
+# @app.get("/")
+# def root():
+#     return{
+#         "name": "Task API",
+#         "version": "1.0", 
+#         "endpoints": ["/tasks"] 
+#     }
+
+
+# @app.get("/health")
+# def health():
+#     return{
+#         "status": "OK"
+#     }
+
+
+#stage-2
+task1 = {"id": 1, "title": "Task 1", "done": False}
+task2 = {"id": 2, "title": "Task 2", "done": True}
+task3 = {"id": 3, "title": "Task 3", "done": False}
+
+tasks = [
+    task1,
+    task2,
+    task3
+]
+
+app = FastAPI()
+
 @app.get("/")
 def root():
     return{
         "name": "Task API",
-        "version": "1.0", 
+        "version": "1.0",  
         "endpoints": ["/tasks"] 
     }
 
+@app.get("/tasks")
+def get_tasks():
+    return tasks
 
-@app.get("/health")
-def health():
-    return{
-        "status": "OK"
-    }
+@app.get("/tasks/{id}")
+def get_task(id: int):  
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    return {"error": f"Task {id} not found"}
