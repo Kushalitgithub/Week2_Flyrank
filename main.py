1305b9bd42d8fb8bd83155f25fae6dcd53efe9ca
Kushal Bhattarai <kushalbhattarai100@gmail.com>
2026-07-20 20:53:35 +0545

    Stage 1: root and health endpoints

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Stage-0
+# @app.get("/")
+
+# def read_root():
+#     return {
+#         "message" : "Hello, World!"
+#         }
+
+# read_root()
+
+#stage-1
 @app.get("/")
+def root():
+    return{
+        "name": "Task API",
+        "version": "1.0", 
+        "endpoints": ["/tasks"] 
+    }
 
-def read_root():
-    return {
-        "message" : "Hello, World!"
-        }
 
-read_root()
+@app.get("/health")
+def health():
+    return{
+        "status": "OK"
+    }
