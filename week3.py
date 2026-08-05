9d02a144a7a2c4a70effa1d5b0f525dec5aaf936
Kushal <kushalbhattarai100@gmail.com>
2026-08-05 15:22:31 +0545

    Stage 3: update and delete with SQL

--- week3.py
+++ week3.py
@@ -33,7 +33,6 @@ def init_db():
         '''
     )
 
-
     cursor.execute("SELECT COUNT(*) FROM tasks")
     count = cursor.fetchone()[0]
 
@@ -45,7 +44,6 @@ def init_db():
                 (task["id"], task["title"], task["done"])
             )
 
-        
     conn.commit()
     conn.close()
 
@@ -60,8 +58,6 @@ def show_tasks():
         print(f"{task[0]}. {task[1]} - {'Done' if task[2] else 'Not Done'}")
 
     conn.close()
-
-
 
 @app.get("/")
 def root():
@@ -130,9 +126,48 @@ def create_task(task: TaskCreate):
         "done": False
     }
 
+@app.put("/tasks/{id}")
+def update_task(id: int, task: TaskUpdate):
+    if task.title.strip() == "":
+        raise HTTPException(status_code=400, detail="Title cannot be empty") 
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM tasks WHERE id = ?", (id,))
+    row = cursor.fetchone()
+
+    if row:
+        cursor.execute("UPDATE tasks SET title = ?, done = ? WHERE id = ?", (task.title, task.done, id))
+        conn.commit()
+        conn.close()
+        return {
+            "id": id,
+            "title": task.title,
+            "done": task.done
+        }
+    else:
+        conn.close()
+        raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+@app.delete("/tasks/{id}")
+def delete_task(id: int):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM tasks WHERE id = ?", (id,))
+    row = cursor.fetchone()
+
+    if row:
+        cursor.execute("DELETE FROM tasks WHERE id = ?", (id,))
+        conn.commit()
+        conn.close()
+        return Response(status_code=204)
+    else:
+        conn.close()
+        raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
 @app.on_event("startup")
 def startup():
     init_db()
-
-
 
